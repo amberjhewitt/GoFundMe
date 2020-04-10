25023ee125f2316ca4efcc2850f78b25c06163ea
Daniel Vogt <danielharlowvogt@gmail.com>
2020-04-09 23:00:01 -0600

    DONE, FINISHED

--- arcticapi/api/views.py
+++ arcticapi/api/views.py
@@ -30,13 +30,15 @@ class CreatePrediction(APIView):
       # aZip = str(body.location_zip)
         
 
-      url = "https://ussouthcentral.services.azureml.net/workspaces/2abd23f891284eb98f5356e46b5cb743/services/5f8fb15e0fa54250a7a827a534139f52/execute?api-version=2.0&details=true"
+      url = "https://ussouthcentral.services.azureml.net/workspaces/2abd23f891284eb98f5356e46b5cb743/services/456a813cb0204680987f80927ad5352c/execute?api-version=2.0&details=true"
 
-      payload = "{\r\n  \"Inputs\": {\r\n    \"input1\": {\r\n      \"ColumnNames\": [\r\n        \"auto_fb_post_mode\",\r\n        \"category_id\",\r\n        \"goal\",\r\n        \"title\",\r\n        \"description\",\r\n        \"location_city\",\r\n        \"location_state\",\r\n        \"location_zip\",\r\n        \"is_charity\",\r\n        \"DonationPerDay\"\r\n      ],\r\n      \"Values\": [\r\n        [\r\n          \"1\",\r\n          \"" + str(body['category_id']) + "\",\r\n          \"" + str(body['goal']) + "\",\r\n          \"" + str(body['title']) + "\",\r\n          \"" + str(body['description']) + "\",\r\n          \"" + str(body['location_city']) + "\",\r\n          \"" + str(body['location_state']) + "\",\r\n          \"" + str(body['location_zip']) + "\",\r\n          \"" + str(body['is_charity']) + "\",\r\n          \"" + str(body['DonationPerDay']) + "\"\r\n        ],\r\n      ]\r\n    }\r\n  },\r\n  \"GlobalParameters\": {}\r\n}"
+      payload = "{\r\n  \"Inputs\": {\r\n    \"input1\": {\r\n      \"ColumnNames\": [\r\n        \"goal\",\r\n        \"title\",\r\n        \"description\",\r\n        \"location_city\",\r\n        \"location_state\",\r\n        \"location_zip\",\r\n        \"is_charity\",\r\n        \"DonationPerDay\"\r\n      ],\r\n      \"Values\": [\r\n        [\r\n          \"" + str(body['goal']) + "\",\r\n          \"" + str(body['title']) + "\",\r\n          \"" + str(body['description']) + "\",\r\n          \"" + str(body['location_city']) + "\",\r\n          \"" + str(body['location_state']) + "\",\r\n          \"" + str(body['location_zip']) + "\",\r\n          \"" + str(body['is_charity']) + "\",\r\n          \"" + str(body['DonationPerDay']) + "\"\r\n        ],\r\n      ]\r\n    }\r\n  },\r\n  \"GlobalParameters\": {}\r\n}"
+
+      
       
       headers = {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer MujZ7ab+kOueM44Y0hTbigu0tG7VGmNkHYts/YRXBBg1fNnCodWvy29uY8UWqxFB9/rNPCBMT5T/HWlOwHIZXg==',
+        'Authorization': 'Bearer AWaqgoZNED0o+NRjTXzz2MNXMmRTaQaUFfhbYDNuXlFgd8uoSHJjCUXSBkw24fSao2TL29MdUBYHdZ/MJuzZ1Q==',
         'Content-Type': 'application/json'
       }
 
